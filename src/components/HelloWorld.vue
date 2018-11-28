c905c2aa88cc62a63a50cd684f7ba31bbd7fb4bf
Vince Banzon <vincebanzon@gmail.com>
2018-11-28 21:05:02 +0800

    Retrieve all chats

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,11 +1,39 @@
 <template>
   <div>
-    
+    <div v-for="message in messages" :key="message">
+      <span>
+        {{ message.message }}
+      </span>
+    </div>
   </div>
 </template>
 
 <script>
+import db from './firebaseInit'
+
 export default {
-  name: 'HelloWorld'
+  name: 'HelloWorld',
+  data() {
+    return {
+      messages: []
+    }
+  },
+  created() {
+    let messagesRef = db.collection('messages').orderBy('timestamp')
+
+    messagesRef.onSnapshot(snapshot => {
+      snapshot.docChanges().forEach((change) => {
+        if(change.type == 'added') {
+          let doc = change.doc
+          this.messages.push({
+            id: doc.id,
+            name: doc.data().name,
+            message: doc.data().message,
+            timestamp: doc.data().timestamp
+          })
+        }
+      })
+    })
+  }
 }
 </script>
